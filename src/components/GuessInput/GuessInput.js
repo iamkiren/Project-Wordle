b6fceda04a58238b56a75793242e10a2de06ddcc
Kiren Raj <iamkiren@gmail.com>
2024-07-23 22:28:58 +0530

    1st Challenge done

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log({ guess });
+
+    setGuess("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        /*the min and max Length is not required but due to some browser error it will not work while using 
+        yoUppercase sometimes here it will be overwritten by the pattern
+        */
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        value={guess}
+        onChange={(event) => {
+          const nextGuess = event.target.value.toUpperCase();
+          setGuess(nextGuess);
+        }}
+        id="guess-input"
+        type="text"
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
